a65887be70b40d4937042b975402f7331656fef3
sirx2713 <bradx2713@gmail.com>
2025-02-07 15:25:53 +0200

    Back C!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 
 int main() {
-    std::string input;
-    std::cout << "Enter name: ";
-    std::cin >> input;
-    std::cout << "Hello " + input  + " !\n" << std::endl;
+    std::string username;
+    int password;
+    std::cout << "Enter username (max 50 characters): ";
+    std::cin >> username;
+    if (username == "admin") {
+        std::cout << "You are a administrator." << std::endl;
+    }
+    else {
+        std::cout << "You are not a administrator." << std::endl;
+        std::cout << "Please enter a password for username: ";
+        std::cin >> password;
+        if (password > 5) {
+            std::cout << "Password is too short." << std::endl;
+            std::cout << "Please enter a password for username: ";
+            std::cin >> password;
+        }
+        else if (password <= 5) {
+            std::cout << "Welcome " + username + " to © iSAD!" << std::endl;
+        }
+    }
 }
